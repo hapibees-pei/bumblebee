f9ceecf9e0dab1c6cf7eb2f382f9cc188ca96bce
0urobor0s <0urobor0s@users.noreply.github.com>
2019-11-09 12:42:00 +0000

    Create User model and setup minimal devise
    Also setup json gem and different json views (jb)

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable,
+  # :rememberable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :validatable
+end
