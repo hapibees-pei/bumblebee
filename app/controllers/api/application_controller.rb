b6c6b07cdb8f566c39206118a8caaff8f1cb7ebc
a-sac <asergioalvesc@gmail.com>
2019-11-25 21:49:45 +0000

    Added \n at EOF; Deleted verify beekeeper

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -4,10 +4,6 @@ module Api
     include ActionController::MimeResponds
     respond_to :json
 
-    def verify_beekeeper
-      render :unauthorized if current_user && !current_user.has_role?(:beekeeper)
-    end
-
     def authenticate_bee!
       unless current_user
         render "api/v1/errors/unauthorized", status: 401
