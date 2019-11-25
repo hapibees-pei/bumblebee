244b08b7ed3cce55128ab0bd704dbf915ed0618e
a-sac <asergioalvesc@gmail.com>
2019-11-25 21:52:51 +0000

    Change Money.locale_backend; Changed user factory name

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     id { SecureRandom.uuid }
-    name { Faker::Lorem.word }
+    name { Faker::Name.name }
     email { 'foo@bar.com' }
     password { 'foobar' }
     address { Faker::Lorem.word }
