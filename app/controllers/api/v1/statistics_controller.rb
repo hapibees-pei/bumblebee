17eb4c1b9a95069527d841fa114dbb790ba501e9
a-sac <asergioalvesc@gmail.com>
2019-12-06 15:18:56 +0000

    Added statistics files/resources

--- app/controllers/api/v1/statistics_controller.rb
+++ app/controllers/api/v1/statistics_controller.rb
@@ -4,6 +4,12 @@ module Api
       before_action :authenticate_bee!
 
       def show
+        sensor_type = params[:sensor_type]
+        hive_uuid = params[:hive_uuid]
+        time_unity = params[:time_unity]
+
+        url_options = "/api/v1/readings?query=#{sensor_type}&hive_id=#{hive_uuid}&group=#{time_unity}"
+        @response = HTTParty.get(Rails.application.credentials.iot_url + url_options)
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
       end
 
       resources :profile, only: :index
+      resources :statistics, only: :show
     end
   end
 
